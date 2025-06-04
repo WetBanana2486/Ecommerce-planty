568ea1fb24909569390ceeece4c31aab2133e718
yuanyu <yfu2486@gmail.com>
2025-06-03 19:42:13 -0500

    Updated ui components & metadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,7 @@
-import Header from '../components/Header/Header';
  
 export default function Page() {
   return (
     <main className="">
-      <Header />
       <p className="font-medium hover:text-blue-400">Blog page</p>
     </main>
   );
