74e18f54003f00ec6c6186d0e5ceea086eb794d2
jason <yfu4@rrc.ca>
2025-06-10 05:01:39 -0500

    Add card component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,62 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Login() {
   return (
     <main className="">
         <p className="font-medium hover:text-blue-400">Login page</p>
-        <Button className="mt-4" variant="default">
-          Login
-        </Button>
+        <Card>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+            <CardAction>Card Action</CardAction>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-700">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mt-4 mb-2 text-sm font-medium text-gray-700">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter>
+            <Button className="mt-4" variant="default">
+              Login
+            </Button>
+            <Button className="mt-4" variant="default">
+              Cancel
+            </Button>
+          </CardFooter>
+        </Card>
+
     </main>
   );
 }
